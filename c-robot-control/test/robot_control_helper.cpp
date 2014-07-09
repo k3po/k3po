ff0e24bfb6db5aa773e2ff1998fbb753a3a48433
Jeffrey Foster <jeffrey.foster@kaazing.com>
2014-07-09 12:44:57 -0700

    Added optional cleanup function to ROBOT_TEST macro

--- c-robot-control/test/robot_control_helper.cpp
+++ c-robot-control/test/robot_control_helper.cpp
@@ -1,12 +1,18 @@
 #include <gtest/gtest.h>
+#include "robot_control_helper.h"
 
 extern "C" {
-    #include "robot_control.h"
+	#include "robot_control.h"
 }
 
-void robotTestWrapper(char * script, void * function, int timeout){
+void robotTestWrapper(char * script, void * function, void * cleanup, int timeout){
 	result * result = robotTest(script, function, timeout);
 	ASSERT_TRUE(result != NULL);
-	ASSERT_STREQ(result->actual_script, result->expected_script);
+	// use expect so can free mem after
+	EXPECT_STREQ(result->expected_script, result->actual_script);
 	robotFree(result);
+	if(cleanup != NULL){
+		((void (*)(void)) cleanup)();
+	}
 }
+
