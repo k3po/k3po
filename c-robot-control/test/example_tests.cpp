c4923a8cdd928ed7797af5371e9d2828445538d7
Jeffrey Foster <jeffrey.foster@kaazing.com>
2014-07-14 15:38:32 -0700

    updated example tests

--- c-robot-control/test/example_tests.cpp
+++ c-robot-control/test/example_tests.cpp
@@ -4,22 +4,63 @@
 
 extern "C" {
     #include "robot_control.h"
+    #include <netinet/in.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <sys/socket.h>
+    #include <string.h>
+    #include <unistd.h>
 }
 
-void clientCode();
+void echo_test();
+void alt_echo_test();
+int socket_connect(in_port_t port);
 
-/* IF YOU HAVE CLIENT CODE TO RUN */
 TEST(Example, Test){
-	/* Arguments: scriptName, functionPointer (function where your client code is), functionPointer (function to any cleanup code you need to run after the clientCode) 
-		Timeout (in seconds, set <= 0 for no timeout) */
-	ROBOT_TEST("exampleScript", clientCode, NULL, 3);
+	/*	Arguments: 
+	**	scriptName (located in ./scripts/, .rpt extension assumed), 
+	**	functionPointer (function where your client code is, NULL if none), 
+	**	functionPointer (function to any cleanup code you need to run after the clientCode, NULL if none) 
+	**	Timeout (in seconds, set <= 0 for no timeout) 
+	*/
+	ROBOT_TEST("exampleScript", echo_test, NULL, 3);
 }
 
-/* IF YOU HAVE NO CLIENT CODE TO RUN */
 TEST(Example, Test2){
-	ROBOT_TEST("exampleScript2", NULL, NULL, -1);
+	ROBOT_TEST("exampleScript2", alt_echo_test, NULL, 2);
 }
 
-void clientCode(){
-	/* ADD CODE HERE */
+void alt_echo_test(){
+	int sock = socket_connect(8001);	
+	char * expected_msg = (char *)"Testing...123";
+	char * recv_msg = (char *)malloc(strlen(expected_msg) + 1);
+	recv(sock, recv_msg, strlen(expected_msg) + 1, 0);
+	ASSERT_STREQ(expected_msg, recv_msg);
+	send(sock, recv_msg, strlen(recv_msg), 0);
+	close(sock);
+	free(recv_msg);
+}
+
+void echo_test(){
+	int sock = socket_connect(8001);
+	char * send_msg = (char *)"Hello, world!";
+	send(sock, send_msg, strlen(send_msg), 0); 	
+	char * recv_msg = (char *)malloc(strlen(send_msg) + 1);
+	recv(sock, recv_msg, strlen(send_msg) + 1, 0);
+	ASSERT_STREQ(send_msg, recv_msg);
+	close(sock);
+	free(recv_msg);
+}
+
+
+int socket_connect(in_port_t port){
+	int sock = socket(PF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in sockaddr;
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_port = htons(port);
+	sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	connect(sock, (struct sockaddr *)&sockaddr, sizeof(sockaddr));
+
+	return sock;
 }
