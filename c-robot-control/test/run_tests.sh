d00feb28a0b6e49c3bbddec1f9b10b863fa9aeed
Jeffrey Foster <jeffrey.foster@kaazing.com>
2014-07-14 17:15:53 -0700

    added script to run examples

---
+++ c-robot-control/test/run_tests.sh
@@ -0,0 +1,21 @@
+mkdir ./tmp
+cd ../../cli/target
+
+# start robot
+nohup java -jar robot.jar start background &> /dev/null &
+sleep 1
+echo Robot Started
+
+# store robot pid
+echo $! >> ../../c-robot-control/test/tmp/robot_pid
+
+# build tests and run
+cd ../../c-robot-control/test
+make
+./example-tests
+
+# kill robot process
+kill -9 $(cat ./tmp/robot_pid)
+
+echo Robot Stopped
+rm -r ./tmp
