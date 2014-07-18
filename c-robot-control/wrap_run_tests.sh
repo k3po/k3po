8f440ffb43925ea0069c65318879ab919d968c4c
Jeffrey Foster <jeff.foster94@gmail.com>
2014-07-18 13:47:42 -0700

    updated output in target directory

--- c-robot-control/wrap_run_tests.sh
+++ c-robot-control/wrap_run_tests.sh
@@ -9,7 +9,11 @@ else
 	if grep --quiet PASSED /vagrant/target/test_results.txt
 		then 
 			echo Tests Passed
-			cp -r /vagrant/lib/libc-robot-control.so /vagrant/target/
+			mkdir /vagrant/target/lib
+			mkdir /vagrant/target/include
+			cp /vagrant/lib/libc-robot-control.so /vagrant/target/lib
+			cp /vagrant/src/robot_control.h /vagrant/target/include/
+			cp /vagrant/src/robot_control_helper.h /vagrant/target/include/
 	else
 		echo Tests Timed Out
 	fi
