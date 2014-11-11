f5bee1dc712189091b5679e3cf5855d63da9800f
John Fallows <john.fallows@kaazing.com>
2014-11-11 10:18:43 -0800

    Add client scripts and execute against server scripts for correctness.

---
+++ src/test/java/org/kaazing/robotic/amqp_1_0/jms/RoboticQueueConsumerIT.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014, Kaazing Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kaazing.robotic.amqp_1_0.jms;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.robot.junit.annotation.Robotic;
+import org.kaazing.robot.junit.rules.RobotRule;
+
+public class RoboticQueueConsumerIT {
+
+    private final RobotRule robot = new RobotRule();
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    @Rule
+    public final TestRule chain = outerRule(robot).around(timeout);
+
+    @Test
+    @Robotic({"client/queue/consumer/create.then.close", "server/queue/consumer/create.then.close"})
+    public void shouldCreateThenClose() throws Exception {
+        robot.join();
+    }
+
+}
