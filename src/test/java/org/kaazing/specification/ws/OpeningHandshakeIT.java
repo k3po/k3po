0853dce1f08590c0b732d6d39cca09b363202103
John Fallows <john.fallows@kaazing.com>
2014-12-28 15:32:50 -0600

    Migrate naming to K3PO

--- src/test/java/org/kaazing/specification/ws/OpeningHandshakeIT.java
+++ src/test/java/org/kaazing/specification/ws/OpeningHandshakeIT.java
@@ -24,8 +24,8 @@ import org.junit.Test;
 import org.junit.rules.DisableOnDebug;
 import org.junit.rules.TestRule;
 import org.junit.rules.Timeout;
-import org.kaazing.k3po.junit.annotation.Robotic;
-import org.kaazing.k3po.junit.rules.RobotRule;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
 
 /**
  * RFC-6455, section 4.1 "Client-Side Requirements"
@@ -33,175 +33,198 @@ import org.kaazing.k3po.junit.rules.RobotRule;
  */
 public class OpeningHandshakeIT {
 
-    private final RobotRule robot = new RobotRule().setScriptRoot("org/kaazing/specification/ws/opening");
+    private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/ws/opening");
 
     private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
 
     @Rule
-    public final TestRule chain = outerRule(robot).around(timeout);
+    public final TestRule chain = outerRule(k3po).around(timeout);
 
     // TODO:
     // proxy => HTTP CONNECT w/ optional authorization, auto-configuration via ws://, wss://
     // TLS (not SSL) w/ SNI for wss://
 
     @Test
-    @Robotic({"connection.established/handshake.request",
-              "connection.established/handshake.response" })
+    @Specification({
+        "connection.established/handshake.request",
+        "connection.established/handshake.response" })
     public void shouldEstablishConnection() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.origin/handshake.request",
-              "request.header.origin/handshake.response" })
+    @Specification({
+        "request.header.origin/handshake.request",
+        "request.header.origin/handshake.response" })
     public void shouldEstablishConnectionWithRequestHeaderOrigin() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.sec.websocket.protocol/handshake.request",
-              "request.header.sec.websocket.protocol/handshake.response" })
+    @Specification({
+        "request.header.sec.websocket.protocol/handshake.request",
+        "request.header.sec.websocket.protocol/handshake.response" })
     public void shouldEstablishConnectionWithRequestHeaderSecWebSocketProtocol() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.sec.websocket.extensions/handshake.request",
-              "request.header.sec.websocket.extensions/handshake.response" })
+    @Specification({
+        "request.header.sec.websocket.extensions/handshake.request",
+        "request.header.sec.websocket.extensions/handshake.response" })
     public void shouldEstablishConnectionWithRequestHeaderSecWebSocketExtensions() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.method.not.get/handshake.request",
-              "request.method.not.get/handshake.response" })
+    @Specification({
+        "request.method.not.get/handshake.request",
+        "request.method.not.get/handshake.response" })
     public void shouldFailHandshakeWhenMethodNotGet() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.version.not.http.1.1/handshake.request",
-              "request.version.not.http.1.1/handshake.response" })
+    @Specification({
+        "request.version.not.http.1.1/handshake.request",
+        "request.version.not.http.1.1/handshake.response" })
     public void shouldFailHandshakeWhenVersionNotHttp11() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.host.missing/handshake.request",
-              "request.header.host.missing/handshake.response" })
+    @Specification({
+        "request.header.host.missing/handshake.request",
+        "request.header.host.missing/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderHostMissing() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.upgrade.missing/handshake.request",
-              "request.header.upgrade.missing/handshake.response" })
+    @Specification({
+        "request.header.upgrade.missing/handshake.request",
+        "request.header.upgrade.missing/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderUpgradeMissing() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.upgrade.not.websocket/handshake.request",
-              "request.header.upgrade.not.websocket/handshake.response" })
+    @Specification({
+        "request.header.upgrade.not.websocket/handshake.request",
+        "request.header.upgrade.not.websocket/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderUpgradeNotWebSocket() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.connection.missing/handshake.request",
-              "request.header.connection.missing/handshake.response" })
+    @Specification({
+        "request.header.connection.missing/handshake.request",
+        "request.header.connection.missing/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderConnectionMissing() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.connection.not.upgrade/handshake.request",
-              "request.header.connection.not.upgrade/handshake.response" })
+    @Specification({
+        "request.header.connection.not.upgrade/handshake.request",
+        "request.header.connection.not.upgrade/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderConnectionNotUpgrade() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.sec.websocket.key.missing/handshake.request",
-              "request.header.sec.websocket.key.missing/handshake.response" })
+    @Specification({
+        "request.header.sec.websocket.key.missing/handshake.request",
+        "request.header.sec.websocket.key.missing/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderSecWebSocketKeyMissing() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.sec.websocket.key.not.16bytes.base64/handshake.request",
-              "request.header.sec.websocket.key.not.16bytes.base64/handshake.response" })
+    @Specification({
+        "request.header.sec.websocket.key.not.16bytes.base64/handshake.request",
+        "request.header.sec.websocket.key.not.16bytes.base64/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderSecWebSocketKeyNot16BytesBase64() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"request.header.sec.websocket.version.not.13/handshake.request",
-              "request.header.sec.websocket.version.not.13/handshake.response" })
+    @Specification({
+        "request.header.sec.websocket.version.not.13/handshake.request",
+        "request.header.sec.websocket.version.not.13/handshake.response" })
     public void shouldFailHandshakeWhenRequestHeaderSecWebSocketVersionNot13() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.connection.not.upgrade/handshake.request",
-              "response.header.connection.not.upgrade/handshake.response" })
+    @Specification({
+        "response.header.connection.not.upgrade/handshake.request",
+        "response.header.connection.not.upgrade/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderConnectionNotUpgrade() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.connection.missing/handshake.request",
-              "response.header.connection.missing/handshake.response" })
+    @Specification({
+        "response.header.connection.missing/handshake.request",
+        "response.header.connection.missing/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderConnectionMissing() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.upgrade.not.websocket/handshake.request",
-              "response.header.upgrade.not.websocket/handshake.response" })
+    @Specification({
+        "response.header.upgrade.not.websocket/handshake.request",
+        "response.header.upgrade.not.websocket/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderUpgradeNotWebSocket() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.upgrade.missing/handshake.request",
-              "response.header.upgrade.missing/handshake.response" })
+    @Specification({
+        "response.header.upgrade.missing/handshake.request",
+        "response.header.upgrade.missing/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderUpgradeMissing() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.sec.websocket.accept.not.hashed/handshake.request",
-              "response.header.sec.websocket.accept.not.hashed/handshake.response" })
+    @Specification({
+        "response.header.sec.websocket.accept.not.hashed/handshake.request",
+        "response.header.sec.websocket.accept.not.hashed/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderSecWebSocketAcceptNotHashed() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.sec.websocket.accept.missing/handshake.request",
-              "response.header.sec.websocket.accept.missing/handshake.response" })
+    @Specification({
+        "response.header.sec.websocket.accept.missing/handshake.request",
+        "response.header.sec.websocket.accept.missing/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderSecWebSocketAcceptMissing() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.sec.websocket.extensions.not.negotiated/handshake.request",
-              "response.header.sec.websocket.extensions.not.negotiated/handshake.response" })
+    @Specification({
+        "response.header.sec.websocket.extensions.not.negotiated/handshake.request",
+        "response.header.sec.websocket.extensions.not.negotiated/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderSecWebSocketExtensionsNotNegotiated() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"response.header.sec.websocket.protocol.not.negotiated/handshake.request",
-              "response.header.sec.websocket.protocol.not.negotiated/handshake.response" })
+    @Specification({
+        "response.header.sec.websocket.protocol.not.negotiated/handshake.request",
+        "response.header.sec.websocket.protocol.not.negotiated/handshake.response" })
     public void shouldFailConnectionWhenResponseHeaderSecWebSocketProtocolNotNegotiated() throws Exception {
-        robot.join();
+        k3po.join();
     }
 
     @Test
-    @Robotic({"multiple.connections.established/handshake.requests",
-              "multiple.connections.established/handshake.responses" })
+    @Specification({
+        "multiple.connections.established/handshake.requests",
+        "multiple.connections.established/handshake.responses" })
     public void shouldEstablishMultipleConnections() throws Exception {
-        robot.join();
+        k3po.join();
     }
 }
