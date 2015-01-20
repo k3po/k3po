f69bed5fde253ac4e61091ab1db191dbca6845a8
kris.mcqueen <kris.mcqueen@kaazing.com>
2015-01-20 11:05:27 -0800

    kaazing/roadmap#40 Implementation of a simple http2 connection negotiation.  The Rupert scripts are fixed up to be symantically correct with respect to reading headers and writing responses, a function is made use of for generating specific HTTP2-Settings, and the scripts are executed against each other in a k3po test.

--- src/test/java/org/kaazing/specification/http2/negotiate/NegotiateHTTP2ConnectionIT.java
+++ src/test/java/org/kaazing/specification/http2/negotiate/NegotiateHTTP2ConnectionIT.java
@@ -16,8 +16,32 @@
 
 package org.kaazing.specification.http2.negotiate;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
 /**
  * HTTP 2 - draft 16, section 3 "Starting HTTP/2"
  */
 public class NegotiateHTTP2ConnectionIT {
+
+    private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/http2/negotiate");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    @Rule
+    public final TestRule chain = outerRule(k3po).around(timeout);
+
+    @Test
+    @Specification({ "negotiate.http2.connection.request", "negotiate.http2.connection.response" })
+    public void shouldEstablishConnection() throws Exception {
+        k3po.join();
+    }
 }
