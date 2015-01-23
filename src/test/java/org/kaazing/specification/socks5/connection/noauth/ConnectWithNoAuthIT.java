2d9074ac23421905a8fd31561703542435c8e245
kris.mcqueen <kris.mcqueen@kaazing.com>
2015-01-22 16:13:22 -0800

    k3po/specification.socks5#1 Add 3 tests for connecting with no authentication required.
      1) Client advertizes that it wants to connect with no authentication required
      2) Client advertizes that it can connect with no authetication, with GSSAPI, or with username/password but server accepts no authetication required
      3) Client advertizes that it wants to connect with no authetication required but server does not accept that method

--- src/test/java/org/kaazing/specification/socks5/connection/noauth/ConnectWithNoAuthIT.java
+++ src/test/java/org/kaazing/specification/socks5/connection/noauth/ConnectWithNoAuthIT.java
@@ -16,8 +16,45 @@
 
 package org.kaazing.specification.socks5.connection.noauth;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
 /**
  * SOCKS5 - RFC 1928, section 3 "Procedure for TCP-based clients" using method 0x00
  */
 public class ConnectWithNoAuthIT {
+    private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/socks5/connection");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    @Rule
+    public final TestRule chain = outerRule(k3po).around(timeout);
+
+    @Test
+    @Specification({ "noauth/connection.noauth.request", "noauth/connection.noauth.response" })
+    public void shouldConnectWithNoAuthRequired() throws Exception {
+        k3po.join();
+    }
+
+    @Test
+    @Specification({ "noauth/connection.noauth.with.multiple.methods.request",
+                     "noauth/connection.noauth.with.multiple.methods.response" })
+    public void shouldConnectWithNoAuthRequiredAndMultipleMethods() throws Exception {
+        k3po.join();
+    }
+
+    @Test
+    @Specification({ "noauth/connection.noauth.with.no.acceptable.methods.request",
+                     "noauth/connection.noauth.with.no.acceptable.methods.response" })
+    public void shouldNotConnectWithNoAuthRequired() throws Exception {
+        k3po.join();
+    }
 }
