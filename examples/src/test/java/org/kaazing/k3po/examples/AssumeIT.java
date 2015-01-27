003f0361cc3e44ec3cb3e01b71b6a70be8b2c4eb
John Fallows <john.fallows@kaazing.com>
2015-01-26 22:52:43 -0800

    Confirm support for JUnit Assume via IT

---
+++ examples/src/test/java/org/kaazing/k3po/examples/AssumeIT.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2014 "Kaazing Corporation," (www.kaazing.com)
+ *
+ * This file is part of Robot.
+ *
+ * Robot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kaazing.k3po.examples;
+
+import static org.junit.Assume.assumeFalse;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
+public class AssumeIT {
+
+    @Rule
+    public K3poRule k3po = new K3poRule();
+
+    @Test
+    @Specification("empty")
+    public void testEmptyAssumptionFailed() throws Exception {
+        assumeFalse(true);
+        k3po.join();
+    }
+}
