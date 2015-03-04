b0052e87e5309ce262fe5ea800dfbb68347c6a13
pkhanal <prashantkhanal@gmail.com>
2015-03-04 14:00:20 -0800

    checkstyle issue resolved

--- launcher/src/main/java/org/kaazing/k3po/launcher/Launcher.java
+++ launcher/src/main/java/org/kaazing/k3po/launcher/Launcher.java
@@ -31,50 +31,58 @@ import org.apache.commons.cli.Parser;
 import org.apache.commons.cli.PosixParser;
 import org.kaazing.k3po.driver.RobotServer;
 
-public class Launcher {
-    
+public final class Launcher {
+
+    private Launcher() {
+        // Added since the checkstyle-plugin is complaining about Utility
+        // classes should not have a public or default constructor.
+    }
+
     public static void main(String... args) throws Exception {
         Options options = createOptions();
-        
+
         try {
             Parser parser = new PosixParser();
             CommandLine cmd = parser.parse(options, args);
             String scriptPath = cmd.getOptionValue("scriptPath");
-                    
+
             File scriptEntryFilePath = new File(scriptPath);
-            
+
             if (!scriptEntryFilePath.exists()) {
                 throw new FileNotFoundException(args[0]);
             }
-            
+
             String controlURI = cmd.getOptionValue("control");
             if (controlURI == null) {
                 controlURI = "tcp://localhost:11642";
             }
-            
+
+            boolean verbose = cmd.hasOption("verbose");
+
             System.out.println("Script Path: " + scriptPath);
             System.out.println("Control URI: " + controlURI);
-            
+
             URL scriptPathEntry = scriptEntryFilePath.toURI().toURL();
-            URLClassLoader scriptLoader = new URLClassLoader(new URL[]{ scriptPathEntry });
-            RobotServer server = new RobotServer(URI.create(controlURI), false, scriptLoader);
+            URLClassLoader scriptLoader = new URLClassLoader(new URL[]{scriptPathEntry});
+            RobotServer server = new RobotServer(URI.create(controlURI), verbose, scriptLoader);
             server.start();
             server.join();
-        }
-        catch (ParseException ex) {
+        } catch (ParseException ex) {
             HelpFormatter helpFormatter = new HelpFormatter();
             helpFormatter.printHelp(Launcher.class.getSimpleName(), options, true);
         }
-        
+
     }
-    
+
     private static Options createOptions() {
         Options options = new Options();
         Option scriptPath = new Option(null, "scriptPath", true, "path to directory/jar for script(s) lookup");
         scriptPath.setRequired(true);
         Option control = new Option(null, "control", true, "location to listen for K3PO control connections");
+        Option verbose = new Option(null, "verbose", false, "verbose");
         options.addOption(scriptPath);
         options.addOption(control);
+        options.addOption(verbose);
         return options;
     }
 }
