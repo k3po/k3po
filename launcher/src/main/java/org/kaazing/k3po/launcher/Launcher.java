82d2e5c82a8b7b3e4e8d451441004a0361efc2cf
pkhanal <prashantkhanal@gmail.com>
2015-03-04 16:38:16 -0800

    added option to accept multiple path entries

--- launcher/src/main/java/org/kaazing/k3po/launcher/Launcher.java
+++ launcher/src/main/java/org/kaazing/k3po/launcher/Launcher.java
@@ -21,6 +21,8 @@ import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
@@ -44,12 +46,16 @@ public final class Launcher {
         try {
             Parser parser = new PosixParser();
             CommandLine cmd = parser.parse(options, args);
-            String scriptPath = cmd.getOptionValue("scriptPath");
+            String scriptPathEntries = cmd.getOptionValue("scriptpath");
+            String[] scriptPathEntryArray = scriptPathEntries.split(";");
+            List<URL> scriptUrls = new ArrayList<URL>();
 
-            File scriptEntryFilePath = new File(scriptPath);
-
-            if (!scriptEntryFilePath.exists()) {
-                throw new FileNotFoundException(args[0]);
+            for (String scriptPathEntry : scriptPathEntryArray) {
+                File scriptEntryFilePath = new File(scriptPathEntry);
+                if (!scriptEntryFilePath.exists()) {
+                    throw new FileNotFoundException(scriptPathEntry);
+                }
+                scriptUrls.add(scriptEntryFilePath.toURI().toURL());
             }
 
             String controlURI = cmd.getOptionValue("control");
@@ -59,11 +65,7 @@ public final class Launcher {
 
             boolean verbose = cmd.hasOption("verbose");
 
-            System.out.println("Script Path: " + scriptPath);
-            System.out.println("Control URI: " + controlURI);
-
-            URL scriptPathEntry = scriptEntryFilePath.toURI().toURL();
-            URLClassLoader scriptLoader = new URLClassLoader(new URL[]{scriptPathEntry});
+            URLClassLoader scriptLoader = new URLClassLoader(scriptUrls.toArray(new URL[0]));
             RobotServer server = new RobotServer(URI.create(controlURI), verbose, scriptLoader);
             server.start();
             server.join();
@@ -76,7 +78,8 @@ public final class Launcher {
 
     private static Options createOptions() {
         Options options = new Options();
-        Option scriptPath = new Option(null, "scriptPath", true, "path to directory/jar for script(s) lookup");
+        Option scriptPath = new Option(null, "scriptpath", true,
+                "Path(s) to directory/jar for script(s) lookup. Multiple entries should be separated by semicolon.");
         scriptPath.setRequired(true);
         Option control = new Option(null, "control", true, "location to listen for K3PO control connections");
         Option verbose = new Option(null, "verbose", false, "verbose");
