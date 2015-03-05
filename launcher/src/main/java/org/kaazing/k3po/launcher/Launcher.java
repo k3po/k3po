58f0c563cbab3f2ad04f1c3f96731e04118c416f
pkhanal <prashantkhanal@gmail.com>
2015-03-04 17:52:52 -0800

    version embedded in the manifest

--- launcher/src/main/java/org/kaazing/k3po/launcher/Launcher.java
+++ launcher/src/main/java/org/kaazing/k3po/launcher/Launcher.java
@@ -17,7 +17,6 @@
 package org.kaazing.k3po.launcher;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -36,8 +35,7 @@ import org.kaazing.k3po.driver.RobotServer;
 public final class Launcher {
 
     private Launcher() {
-        // Added since the checkstyle-plugin is complaining about Utility
-        // classes should not have a public or default constructor.
+        // no instances
     }
 
     public static void main(String... args) throws Exception {
@@ -46,15 +44,17 @@ public final class Launcher {
         try {
             Parser parser = new PosixParser();
             CommandLine cmd = parser.parse(options, args);
-            String scriptPathEntries = cmd.getOptionValue("scriptpath");
+
+            if (cmd.hasOption("version")) {
+                System.out.println("Version: " + Launcher.class.getPackage().getImplementationVersion());
+            }
+
+            String scriptPathEntries = cmd.getOptionValue("scriptpath", "src/test/scripts");
             String[] scriptPathEntryArray = scriptPathEntries.split(";");
             List<URL> scriptUrls = new ArrayList<URL>();
 
             for (String scriptPathEntry : scriptPathEntryArray) {
                 File scriptEntryFilePath = new File(scriptPathEntry);
-                if (!scriptEntryFilePath.exists()) {
-                    throw new FileNotFoundException(scriptPathEntry);
-                }
                 scriptUrls.add(scriptEntryFilePath.toURI().toURL());
             }
 
@@ -80,12 +80,13 @@ public final class Launcher {
         Options options = new Options();
         Option scriptPath = new Option(null, "scriptpath", true,
                 "Path(s) to directory/jar for script(s) lookup. Multiple entries should be separated by semicolon.");
-        scriptPath.setRequired(true);
         Option control = new Option(null, "control", true, "location to listen for K3PO control connections");
         Option verbose = new Option(null, "verbose", false, "verbose");
+        Option version = new Option(null, "version", false, "version");
         options.addOption(scriptPath);
         options.addOption(control);
         options.addOption(verbose);
+        options.addOption(version);
         return options;
     }
 }
