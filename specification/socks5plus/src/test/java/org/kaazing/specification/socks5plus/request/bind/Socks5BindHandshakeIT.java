669bcfdc850cd727aa77f425c3492ec63aa59a06
Alin Gherasim <agherasim@luxoft.com>
2015-09-08 17:19:28 +0300

    Added tests for CONNECT and BIND requests.

--- specification/socks5plus/src/test/java/org/kaazing/specification/socks5plus/request/bind/Socks5BindHandshakeIT.java
+++ specification/socks5plus/src/test/java/org/kaazing/specification/socks5plus/request/bind/Socks5BindHandshakeIT.java
@@ -16,8 +16,53 @@
 
 package org.kaazing.specification.socks5plus.request.bind;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
 /**
  * SOCKS5 - RFC 1928, section 4 "Requests" and section 6 "Replies"
  */
 public class Socks5BindHandshakeIT {
+    private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/socks5plus/request");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    @Rule
+    public final TestRule chain = outerRule(k3po).around(timeout);
+
+    @Test
+    @Specification({
+        "bind/connection.request.bind.with.uri.valid.port.succeeded.request",
+        "bind/connection.request.bind.with.uri.valid.port.succeeded.response"
+        })
+    public void shouldBindWithUriAndValidPortSucceeded() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "bind/connection.request.bind.with.uri.invalid.port.request",
+        "bind/connection.request.bind.with.uri.invalid.port.response"
+        })
+    public void shouldNotBindWithUriAndInvalidPort() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "bind/connection.request.bind.with.invalid.uri.request",
+        "bind/connection.request.bind.with.invalid.uri.response"
+        })
+    public void shouldNotBindWithInvalidUri() throws Exception {
+        k3po.finish();
+    }
+
 }
--- specification/socks5plus/src/test/java/org/kaazing/specification/socks5plus/request/connect/Socks5ConnectHandshakeIT.java
+++ specification/socks5plus/src/test/java/org/kaazing/specification/socks5plus/request/connect/Socks5ConnectHandshakeIT.java
@@ -16,8 +16,53 @@
 
 package org.kaazing.specification.socks5plus.request.connect;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
 /**
  * SOCKS5 - RFC 1928, section 4 "Requests" and section 6 "Replies"
  */
 public class Socks5ConnectHandshakeIT {
+    private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/socks5plus/request");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    @Rule
+    public final TestRule chain = outerRule(k3po).around(timeout);
+
+    @Test
+    @Specification({
+        "connect/connection.request.connect.with.uri.valid.port.succeeded.request",
+        "connect/connection.request.connect.with.uri.valid.port.succeeded.response"
+        })
+    public void shouldConnectWithUriAndValidPortSucceeded() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "connect/connection.request.connect.with.uri.invalid.port.request",
+        "connect/connection.request.connect.with.uri.invalid.port.response"
+        })
+    public void shouldNotConnectWithUriAndInvalidPort() throws Exception {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "connect/connection.request.connect.with.invalid.uri.request",
+        "connect/connection.request.connect.with.invalid.uri.response"
+        })
+    public void shouldNotConnectWithInvalidUri() throws Exception {
+        k3po.finish();
+    }
+
 }
