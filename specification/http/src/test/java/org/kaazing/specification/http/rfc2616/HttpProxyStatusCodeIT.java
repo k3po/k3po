0034f60b4cec6bc8f96d0659307c15b7a017ec06
Mihai Salavastru <msalavastru@luxoft.com>
2015-10-27 18:06:45 +0200

    Added test case scripts for proxy should return 504 Gateway Timeout when server is down

---
+++ specification/http/src/test/java/org/kaazing/specification/http/rfc2616/HttpProxyStatusCodeIT.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2007-2014 Kaazing Corporation. All rights reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kaazing.specification.http.rfc2616;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
+/**
+ * Test to validate behavior as specified in <a href="https://tools.ietf.org/html/rfc7230#section-6">RFC 7230 section 6:
+ * Connection Management</a>.
+ */
+public class HttpProxyStatusCodeIT {
+
+    private final K3poRule k3po = new K3poRule().setScriptRoot("org/kaazing/specification/http/rfc2616/status.code");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    @Rule
+    public final TestRule chain = outerRule(k3po).around(timeout);
+
+    @Test
+    @Specification({
+            "proxy.should.return.504.response.when.server.is.down/client",
+            "proxy.should.return.504.response.when.server.is.down/proxy",
+            "proxy.should.return.504.response.when.server.is.down/server" })
+    public void proxyShouldReturn504ResponseWhenServerIsDown() throws Exception {
+        k3po.finish();
+    }
+}
