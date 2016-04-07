d2466ab8a975252b63d2de9996c095086c01db9f
David Witherspoon <david.witherspoon@kaazing.com>
2016-04-06 17:29:08 -0700

    Added same catch clause to shutdown of server pipelines

--- driver/src/main/java/org/kaazing/k3po/driver/internal/Robot.java
+++ driver/src/main/java/org/kaazing/k3po/driver/internal/Robot.java
@@ -354,15 +354,19 @@ public class Robot {
 
             // clear out the pipelines for new connections to avoid impacting the observed script
             for (ServerBootstrapResolver serverResolver : configuration.getServerResolvers()) {
-                ServerBootstrap server = serverResolver.resolve();
-                server.setPipelineFactory(pipelineFactory(pipeline(closeOnExceptionHandler)));
+                try {
+                    ServerBootstrap server = serverResolver.resolve();
+                    server.setPipelineFactory(pipelineFactory(pipeline(closeOnExceptionHandler)));
+                } catch (RuntimeException e) {
+                    LOGGER.warn("Exception caught while trying to stop server pipelies", e);
+                }
             }
             for (ClientBootstrapResolver clientResolver : configuration.getClientResolvers()) {
                 try {
                     ClientBootstrap client = clientResolver.resolve();
                     client.setPipelineFactory(pipelineFactory(pipeline(closeOnExceptionHandler)));
                 } catch (RuntimeException e) {
-                    LOGGER.warn("Exception caught while trying to stop client pipelies: " + e);
+                    LOGGER.warn("Exception caught while trying to stop client pipelies", e);
                 }
             }
 
