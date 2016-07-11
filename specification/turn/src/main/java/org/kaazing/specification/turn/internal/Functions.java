fee1bcf29d7e98d1fce911f09224b855ceb4af1d
a-zuckut <zuckutalex@yahoo.com>
2016-07-11 11:46:47 -0700

    Working TURN Protocol! (have a wireshark)

--- specification/turn/src/main/java/org/kaazing/specification/turn/internal/Functions.java
+++ specification/turn/src/main/java/org/kaazing/specification/turn/internal/Functions.java
@@ -27,27 +27,38 @@ import org.kaazing.k3po.lang.el.spi.FunctionMapperSpi;
 public final class Functions {
 
     private static final Random RANDOM = new Random();
-    
+
     @Function
-    public static byte[] ipAddressXORWithMagicCookie(String ip) {
+    public static byte[] portXOR(int port) {
+        byte[] x = new byte[2];
+        byte one = (byte)(port >> 8);
+        byte two = (byte)(port);
+        x[0] = (byte)(one ^ 0x21); // magic cookie byte 1
+        x[1] = (byte)(two ^ 0x12); // magic cookie byte 2
+        return x;
+    }
+
+    @Function
+    public static byte[] ipXOR(String ip) {
         byte[] x = new byte[4];
-        byte[] holder = new byte[4];
-        try{
-            String[] ints = ip.split(".");
-            for(int i = 0; i < holder.length; i++) {
-                holder[i] = Byte.parseByte(ints[i]);
-            }
-        }catch(Exception e) {
+        int[] temp = new int[4];
+        byte[] temp2 = new byte[4];
+
+        String[] string = ip.split("\\.");
+        for (int i = 0; i < temp.length; i++) {
+            temp2[i] = (byte)Integer.parseInt(string[i]);
         }
-        byte magic_cookie_0 = 0x21;
-        byte magic_cookie_1 = 0x12;
-        byte magic_cookie_2 = (byte)(0xA4);
-        byte magic_cookie_3 = 0x42;
-        
-        x[0] = (byte)(magic_cookie_0 ^ holder[0]);
-        x[0] = (byte)(magic_cookie_1 ^ holder[1]);
-        x[0] = (byte)(magic_cookie_2 ^ holder[2]);
-        x[0] = (byte)(magic_cookie_3 ^ holder[3]);
+
+        byte magic_cookie_0 = (byte)0x21;
+        byte magic_cookie_1 = (byte)0x12;
+        byte magic_cookie_2 = (byte)0xA4;
+        byte magic_cookie_3 = (byte)0x42;
+
+        x[0] = (byte) (magic_cookie_0 ^ temp2[0]);
+        x[1] = (byte) (magic_cookie_1 ^ temp2[1]);
+        x[2] = (byte) (magic_cookie_2 ^ temp2[2]);
+        x[3] = (byte) (magic_cookie_3 ^ temp2[3]);
+
         return x;
     }
 
