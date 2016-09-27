ca0acfe4c0ff771c549296bf693f488d0c60dcb6
NicoletaOita <nicoleta.oita@kaazing.com>
2016-09-27 19:00:31 +0300

    Test message digest (#367)
    * Added tests for invalid magic cookie and transaction id.
    
    * Added test for invalid requested transport attribute.
    
    * Added tests for ChannelData messages.
    
    * Added a second ChannelData message in order to see a difference in the
    k3po response.
    
    * Added some more tests for turn proxy.
    
    * Ignored tests for which there are open issues.
    
    * Fixed test that should discard message.
    
    * Ignored tests for which there are open issues.
    
    * Removed some comments.
    
    * Added k3po turn server tests.
    
    * Fixed k3po tests for ChannelData.
    
    * Fixed spelling error.
    
    * Fixed script for invalid magic cookie.
    
    * Added test for message digest.
    
    * Adjusted test for message digest to match the realm name from
    TurnFrameEncoder.
    
    * Removed the unnecessary padding.
    
    * Split invalid magic cookie test into two different tests, for udp and tcp.
    
    * Reorganized tests.
    
    * Fixed small mistake.

--- specification/turn/src/test/java/org/kaazing/specification/turn/AllocationsIT.java
+++ specification/turn/src/test/java/org/kaazing/specification/turn/AllocationsIT.java
@@ -74,12 +74,6 @@ public class AllocationsIT {
         k3po.finish();
     }
 
-    @Test
-    @Specification({"incorrect.allocation.method.wrong.magic.cookie/request", "incorrect.allocation.method.wrong.magic.cookie/response" })
-    public void shouldFailWithIncorrectMagicCookie() throws Exception {
-        k3po.finish();
-    }
-
     /**
      * See <a href="https://tools.ietf.org/html/rfc5766#section-6">RFC 5766 section 6: Allocations</a>.
      */
@@ -143,6 +137,15 @@ public class AllocationsIT {
     @Specification({"no.requested.transport.attribute.responds.400/request",
             "no.requested.transport.attribute.responds.400/response"})
     public void shouldRespond400ToAllocateWithNoRequestedTransportAttribute() throws Exception {
+        k3po.finish();
+    }
+
+    /**
+     * See <a href="https://tools.ietf.org/html/rfc5766#section-6">RFC 5766 section 6: Allocations</a>.
+     */
+    @Test
+    @Specification({"check.message.digest/request", "check.message.digest/response" })
+    public void shouldSucceedWithCorrectMessageDigest() throws Exception {
         k3po.finish();
     }
 
